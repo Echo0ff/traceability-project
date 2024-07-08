cb70fe95887209135c21bcce41d3eddeece200d0
EchoOff <728979340@qq.com>
2024-07-08 20:41:39 +0800

    "Stop tracking nginx directory"

--- nginx/nginx-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Substitute environment variables in the nginx configuration template
-envsubst '\$DOMAIN' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Start nginx
-nginx -g 'daemon off;'
